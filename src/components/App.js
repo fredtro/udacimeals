3c9b7dc8c6190857ab106248f00e607db5e5338e
Frederik Trostorf <fredtrostorf@gmail.com>
2017-12-18 20:48:39 +0100

    feature: Use mapDispatchToProps to add action invokation as prop function

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
+
   render() {
-    //console.log(this.props)
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+    console.log(this.props)
+    return <div>Hello World</div>;
   }
 }
 
-function mapStateToProps(calendar){
-  console.log(calendar);
-  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+function mapStateToProps(calendar) {
+  const dayOrder = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+  ];
 
   return {
-    calendar : dayOrder.map((day) => ({
+    calendar: dayOrder.map(day => ({
       day,
       meal: Object.assign({}, calendar)
     }))
-  }
-
+  };
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch){
+  return {
+    selectRecipe : (data) => dispatch(addRecipe(data)),
+    remove : (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps)(App);
